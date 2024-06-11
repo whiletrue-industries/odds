4ff35fa6ab268702263fb0f34784f1a2f88e77a5
Adam Kariv <adam.kariv@gmail.com>
2024-06-11 13:46:37 +0300

    Scrape 1

--- utils/scrape_one.py
+++ utils/scrape_one.py
@@ -1,4 +1,5 @@
+import sys
 from odds.backend import backend
 b = backend.ODDSBackend()
-b.scan_specific(datasetId='uk/gp-prescribing-data')
+b.scan_specific(catalogId=sys.argv[1], datasetId=sys.argv[2])
 del b
