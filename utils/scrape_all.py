ef861b15a7137f2a9857c6146e57c3d030e5fbf5
Adam Kariv <adam.kariv@gmail.com>
2025-01-30 21:24:47 +0200

    Allow scraping only one source

--- utils/scrape_all.py
+++ utils/scrape_all.py
@@ -1,4 +1,11 @@
 from odds.backend import backend
+import sys
+
 b = backend.ODDSBackend()
-b.scan_required()
+
+if len(sys.argv) > 1:
+    selected_catalog = sys.argv[1]
+    b.scan_specific(catalogId=selected_catalog)
+else:
+    b.scan_required()
 del b
--- utils/scrape_all.sh
+++ utils/scrape_all.sh
@@ -1,4 +1,4 @@
 docker run -it \
     -v ./odds.config.yaml:/srv/odds.config.yaml \
     -v ./cache/:/srv/.caches/ \
-    ghcr.io/whiletrue-industries/odds/odds-server:latest -- "python utils/scrape_all.py"
+    ghcr.io/whiletrue-industries/odds/odds-server:latest -- "python utils/scrape_all.py $@"
