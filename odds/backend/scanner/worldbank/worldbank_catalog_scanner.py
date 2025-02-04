2f5743b45cc227f952e40ba536c7f2d691382d07
Adam Kariv <adam.kariv@gmail.com>
2025-02-04 14:04:33 +0200

    WB scanner

--- odds/backend/scanner/worldbank/worldbank_catalog_scanner.py
+++ odds/backend/scanner/worldbank/worldbank_catalog_scanner.py
@@ -26,6 +26,7 @@ class WorldBankCatalogScanner(CatalogScanner):
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:124.0) Gecko/20100101 Firefox/124.0'
             }
             headers.update(self.catalog.http_headers)
+            dataset_ids = []
             while True:
                 if config.debug:
                     rts.set(self.ctx, f"Getting skip {skip} of datasets from worldbank api")
@@ -40,50 +41,44 @@ class WorldBankCatalogScanner(CatalogScanner):
                 except Exception as e:
                     rts.set(self.ctx, f"Error getting skip {skip} of datasets from worldbank api: {e!r}", 'error')
                     raise
-                dataset_ids = r['data']
-                if len(dataset_ids) == 0:
+                data = r.get('data', [])
+                dataset_ids.extend(data)
+                if len(data) == 0 or self.done(len(dataset_ids)):
                     break
-                datasets = []
-                for dataset_id in dataset_ids:
-                    dataset_unique_id = dataset_id['dataset_unique_id']
-                    try:
-                        r = await Retry()(client, 'get',
-                            f"https://datacatalogapi.worldbank.org/ddhxext/DatasetView", params={"dataset_unique_id": dataset_unique_id},
-                            headers=headers,
-                            timeout=240
-                        )
-                        r.raise_for_status()
-                        r = r.json()
-                        datasets.append(r)
-                    except Exception as e:
-                        rts.set(self.ctx, f"Error getting dataset {dataset_unique_id} from worldbank api: {e!r}", 'error')
-                        raise
-
-                for dataset in datasets:
-                    resources = [
-                        Resource(
-                            resource['url'],
-                            resource['url'].split('?')[0].split('.')[-1],
-                            title=','.join(filter(None, [resource.get('name'), resource.get('description')])),
-                        )
-                        for resource in dataset['Resources']
-                        if resource.get('url')
-                    ]
-                    if len(resources) == 0:
-                        continue
-                    identification = dataset.get('identification', {})
-                    dataset = Dataset(
-                        self.catalog.id, 
-                        dataset['dataset_unique_id'], dataset['name'], 
-                        description='\n'.join(filter(None, [identification.get('title'), identification.get('subtitle'), identification.get('description')])),
-                        publisher=identification.get('citation'),
-                        publisher_description='',
-                        resources=resources
+                skip += len(data)
+            for dataset_id in dataset_ids:
+                dataset_unique_id = dataset_id['dataset_unique_id']
+                try:
+                    r = await Retry()(client, 'get',
+                        f"https://datacatalogapi.worldbank.org/ddhxext/DatasetView", params={"dataset_unique_id": dataset_unique_id},
+                        headers=headers,
+                        timeout=240
                     )
-                    yield dataset
-                    num_rows += 1
-                    if self.done(num_rows):
-                        break
-                if self.done(num_rows):
-                    break
-                page += 1
+                    r.raise_for_status()
+                    r = r.json()
+                    dataset = r
+                except Exception as e:
+                    rts.set(self.ctx, f"Error getting dataset {dataset_unique_id} from worldbank api: {e!r}", 'error')
+                    raise
+
+                resources = [
+                    Resource(
+                        resource['url'],
+                        resource['url'].split('?')[0].split('.')[-1],
+                        title=','.join(filter(None, [resource.get('name'), resource.get('description')])),
+                    )
+                    for resource in dataset['Resources']
+                    if resource.get('url')
+                ]
+                if len(resources) == 0:
+                    continue
+                identification = dataset.get('identification', {})
+                dataset = Dataset(
+                    self.catalog.id, 
+                    dataset['dataset_unique_id'], dataset['name'], 
+                    description='\n'.join(filter(None, [identification.get('title'), identification.get('subtitle'), identification.get('description')])),
+                    publisher=identification.get('citation'),
+                    publisher_description='',
+                    resources=resources
+                )
+                yield dataset
