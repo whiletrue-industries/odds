d2764441c108b4cc8dd0127ba238f3c003099d5c
Adam Kariv <adam.kariv@gmail.com>
2025-03-28 12:54:56 +0300

    Fix scrape all 'force' flag

--- utils/scrape_all.py
+++ utils/scrape_all.py
@@ -10,7 +10,7 @@ async def main():
     parser = argparse.ArgumentParser(description='Scrape data from all catalogs')
     parser.add_argument('--catalog-id', type=str, help='ID of the catalog to scrape')
     parser.add_argument('--dataset-id', type=str, help='ID of the dataset to scrape')
-    parser.add_argument('--force', type=bool, default=False, help='Scrape all and not just missing datasets')
+    parser.add_argument('--force', action='store_true', help='Scrape all and not just missing datasets')
     args = parser.parse_args()
 
     redis = await create_pool(REDIS_SETTINGS)
