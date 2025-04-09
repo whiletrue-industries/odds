d58ab26f8b51bf7b762072da068b7db41e6421ed
Adam Kariv <adam.kariv@gmail.com>
2025-04-09 13:39:18 +0300

    Embed code

--- frontend/ask/projects/ask/public/embed/odds.js
+++ frontend/ask/projects/ask/public/embed/odds.js
@@ -1,4 +1,4 @@
-let html = `
+var odds___html = `
 <div id="odds" style="position: fixed; bottom: 16px; left: 16px; z-index: 9999;">
     <div class='trigger'>
         <span>חיפוש מידע</span>
@@ -8,7 +8,7 @@ let html = `
 </div>
 `;
 
-let css = `
+let odds___css = `
 #odds {
     z-index: 9999;
     position: fixed;
@@ -62,17 +62,17 @@ let css = `
 
 `;
 
-let onload = null;
-odds_load = function() {
-    onload ? onload() : null;
+var odds___onload = null;
+odds___load = function() {
+    odds___onload ? odds___onload() : null;
     var body = document.getElementsByTagName('body')[0];
     console.log(body);
     // Create element in body based on html, add css to head
     var div = document.createElement('div');
-    div.innerHTML = html;
+    div.innerHTML = odds___html;
     body.appendChild(div);
     var style = document.createElement('style');
-    style.innerHTML = css;
+    style.innerHTML = odds___css;
     document.head.appendChild(style);
     // Load font
     var link = document.createElement('link');
@@ -95,9 +95,9 @@ odds_load = function() {
     
 if (document.readyState === "complete") {
     console.log('Loading odds post load');
-    odds_load();
+    odds___load();
 } else {
     console.log('Loading odds');
-    onload = window.onload;
-    window.onload = odds_load;
+    odds___onload = window.onload;
+    window.onload = odds___load;
 }
